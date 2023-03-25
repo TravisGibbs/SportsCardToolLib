cf51394bd67a751663bf86fbed6949da971b7653
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-03-25 12:54:50 -0400

    add grab sales to init.py

--- SportsCardTool/__init__.py
+++ SportsCardTool/__init__.py
@@ -1,2 +1,2 @@
 from ._version import __version__
-from .scraping_tool import filter_hrefs, get_soup, parse_panel, grab_card_list, dump_data, grab_year_links
+from .scraping_tool import filter_hrefs, get_soup, parse_panel, grab_card_list, dump_data, grab_year_links, grab_sales
