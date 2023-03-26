6a503636796eece45156e11a56330373d090eea5
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-03-26 19:20:07 -0400

    Move tests and reimport

--- SportsCardTool/tests/test_all.py
+++ SportsCardTool/tests/test_all.py
@@ -1,4 +1,4 @@
-from SportsCardTool import grab_card_list, dump_data, get_soup, filter_hrefs, grab_year_links, QueryBuilder
+from SportsCardTool import grab_card_list, dump_data, get_soup, filter_hrefs, grab_year_links, grab_sales, QueryBuilder
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -16,16 +16,16 @@ def test_grab_year_links():
     assert len(grab_year_links(["2023", "2022"])) == 2
 
 
+def test_grab_sales():
+    assert len(grab_sales("2022 Mike Trout")) > 0
+    assert len(grab_sales("2019 Shohei Ohtani")) > 0
+
+
 def test_grab_data():
     card_list = grab_card_list(grab_year_links(["2023"]))
     assert type(card_list) == type(list())
     assert len(card_list) > 100
-    assert isinstance(card_list[0], dict)
-
-
-def test_grab_sales():
-    assert len(grab_sales("2022 Mike Trout")) > 0
-    assert len(grab_sales("2019 Shohei Ohtani")) > 0
+    assert type(card_list[0]) == type(dict())
 
 
 def test_dump_date():
